78f843418a35d6b5d8cb7b4ad37432074a3963de
Henry211 <Henry211@users.noreply.github.com>
2020-01-04 00:34:53 -0600

    Adición de Clases

---
+++ ProyectoProgramacionII/ProyectoProgramacionII/UNA/Elementos/CuadernoDigital.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProyectoProgramacionII
+{
+    public class CuadernoDigital
+    {
+        public int nombre
+        {
+            get;
+            set;
+        }
+
+        public int categoria
+        {
+            get;
+            set;
+        }
+
+        public int color
+        {
+            get;
+            set;
+        }
+
+        public int orden
+        {
+            get;
+            set;
+        }
+
+        public int notes
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ ProyectoProgramacionII/ProyectoProgramacionII/UNA/Elementos/Nota.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProyectoProgramacionII
+{
+    public class Nota
+    {
+        public int titulo
+        {
+            get;
+            set;
+        }
+
+        public int privacidad
+        {
+            get;
+            set;
+        }
+
+        public int categoria
+        {
+            get;
+            set;
+        }
+
+        public int color
+        {
+            get;
+            set;
+        }
+
+        public int fuente
+        {
+            get;
+            set;
+        }
+
+        public int letraColor
+        {
+            get;
+            set;
+        }
+
+        public int orden
+        {
+            get;
+            set;
+        }
+
+        public int fechaDeCreacion
+        {
+            get;
+            set;
+        }
+
+        public int fechaDeModificacion
+        {
+            get;
+            set;
+        }
+    }
+}
