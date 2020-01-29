4d592ae7ab5b957ff4311e0672555835c00758f9
Henry211 <Henry211@users.noreply.github.com>
2020-01-28 22:17:18 -0600

    Implementacion de conección a DataBase

---
+++ ProyectoProgramacionII/Biblioteca/Biblioteca/Clases/DBAccess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+
+namespace Biblioteca.Biblioteca.Clases
+{
+    public abstract class DBAccess
+    {
+        public string ConnectionString { set; get; }
+        public IDbTransaction Transaction { set; get; }
+        public IDbConnection Connection { set; get; }
+
+        public abstract void OpenConnection();
+        public abstract void CloseConnection();
+        public abstract long EjectSQL(string a);
+        public abstract DataTable QuerySQL(string b);
+        public abstract Boolean IsTransaction();
+        public abstract void CommitTransaction();
+        public abstract void RollBackTransaction();
+        public abstract void BeginTransaction();
+
+    }
+}
---
+++ ProyectoProgramacionII/Biblioteca/Biblioteca/Clases/MysqlAccess.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Biblioteca.Biblioteca.Clases
+{
+    public class MysqlAccess : DBAccess
+    {
+        public override void OpenConnection()
+        {
+            Connection = new MySqlConnection(ConnectionString);
+            Connection.Open();
+        }
+        public override void CloseConnection()
+        {
+            if (Connection != null)
+            {
+                Connection.Close();
+            }
+        }
+        public override long EjectSQL(string sql)
+        {
+            MySqlCommand mySqlCommand = new MySqlCommand(sql, (MySqlConnection)Connection);
+            return mySqlCommand.ExecuteNonQuery();
+
+        }
+        public override DataTable QuerySQL(string sql)
+        {
+            DataTable result = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(sql, (MySqlConnection)Connection);
+            adapter.Fill(result);
+            return result;
+        }
+        public override Boolean IsTransaction()
+        {
+            return (Transaction != null);
+
+        }
+        public override void CommitTransaction()
+        {
+            if (IsTransaction())
+            {
+                Transaction.Commit();
+            }
+        }
+        public override void RollBackTransaction()
+        {
+            if (IsTransaction())
+            {
+                Transaction.Rollback();
+            }
+        }
+        public override void BeginTransaction()
+        {
+            if (Connection != null)
+            {
+                Transaction = Connection.BeginTransaction();
+            }
+        }
+    }
+}
